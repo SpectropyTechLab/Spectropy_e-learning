8e2d0f5f8c59ee768df05086ed922bf26737fda2
Rahul Viswa <turnerevent.co@gmail.com>
2025-11-13 14:53:11 +0530

    third deployment commit

--- backend/server.js
+++ backend/server.js
@@ -64,7 +64,7 @@ process.on('unhandledRejection', (reason) => {
   console.error('Unhandled Rejection:', reason);
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
 
   console.log(`Server running on http://localhost:${PORT}`);
 });
