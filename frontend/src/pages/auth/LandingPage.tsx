b892667fdb322dd66dc23ae97cfc2d8df24be1e4
Poojitha Chandragiri <poojithachandragiri903@gmail.com>
2026-01-10 19:47:00 +0530

    future foundation updated graphics

--- frontend/src/pages/auth/LandingPage.tsx
+++ frontend/src/pages/auth/LandingPage.tsx
@@ -6,7 +6,7 @@ import BlogPage from './BlogPage';
 import FutureFoundationSlider from './FutureFoundationSlider';
 import HappyClientsSlider from './HappyClientsSlider';
 import ChallengesModal from '../../components/Landingpage/challenges';
-import Programfuturefoundation from './programff';
+import Programfuturefoundation from './FFProgram';
 // --- SVGs for Icons ---
 const MenuIcon = () => (
     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
